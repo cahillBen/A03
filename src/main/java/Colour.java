5833c50ebe11a71b95ff74c69646e511eb6dde66
cahillBen <98129211+cahillBen@users.noreply.github.com>
2022-11-25 18:36:22 +0000

    created constructor which assigned r attribute from input

---
+++ src/main/java/Colour.java
@@ -0,0 +1,7 @@
+public class Colour {
+
+    double r;
+    public Colour(double r, double g, double b){
+        this.r = r;
+    }
+}
--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -6,6 +6,6 @@ public class ColourTest {
     @Test
     public void colourRedParameterTest(){
         Colour red = new Colour(1.0, 0.5, 0.0);
-        assertEquals(red.r, 1.0);
+        assertEquals(red.r, 1.0, 0.0);
     }
 }
