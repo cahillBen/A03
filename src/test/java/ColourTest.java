159b339493e3fe3ac5161fd1d6c7fc57d81881ee
cahillBen <98129211+cahillBen@users.noreply.github.com>
2022-11-25 18:47:27 +0000

    test Colour green has attribute g with value 0.5

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -8,4 +8,10 @@ public class ColourTest {
         Colour red = new Colour(1.0, 0.5, 0.0);
         assertEquals(red.r, 1.0, 0.0);
     }
+
+    @Test
+    public void colourGreenParameterTest(){
+        Colour green = new Colour(0.0, 0.5, 0.0);
+        assertEquals(green.g, 0.5, 0.0);
+    }
 }
