6dc479bc1fe8c33241325c708c70d864277e3ef9
cahillBen <98129211+cahillBen@users.noreply.github.com>
2022-11-29 10:41:56 +0000

    Added javadocs to Colour class

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,8 +1,18 @@
 public class Colour {
+    // RGB values of colour
+    // represented as values 0 to 255
+    private final double r, g, b;
 
-    private final double r;
-    private final double g;
-    private final double b;
+    /**
+     * Constructor taking 3 values representing
+     * amount of r, g and b in the colour
+     * Must be values between 0 and 1
+     * each converted to a value 0 to 255
+     *
+     * @param r amount of red in the colour
+     * @param g amount of green in the colour
+     * @param b amount of blue in the colour
+     */
     public Colour(double r, double g, double b){
         if (r < 0 || r > 1) {
             throw new IllegalArgumentException("'r' input is out of range");
@@ -18,12 +28,25 @@ public class Colour {
         this.b = b * 255;
     }
 
+    /**
+     * single binary input constructor
+     * takes a list of 3 bytes and converts these to RGB values
+     *
+     * @param rgb list with 3 binary inputs
+     */
     public Colour(int[] rgb){
         this.r = rgb[0];
         this.g = rgb[1];
         this.b = rgb[2];
     }
 
+    /**
+     * Compare Colour object with another object
+     * True if 'o' is a Colour object with the same RGB values
+     *
+     * @param o     Any object to be compared
+     * @return boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -32,15 +55,32 @@ public class Colour {
         return Double.compare(colour.r, r) == 0 && Double.compare(colour.g, g) == 0 && Double.compare(colour.b, b) == 0;
     }
 
+    /**
+     * Getter for 'r' value
+     *
+     * @return  r     The colour’s r value.
+     */
     public double getR() {
         return r;
     }
 
+    /**
+     * Getter for 'g' value
+     *
+     * @return  g     The colour’s g value.
+     */
+    public double getG() {
+        return g;
+    }
+
+    /**
+     * Getter for 'b' value
+     *
+     * @return  b     The colour’s b value.
+     */
     public double getB() {
         return b;
     }
 
-    public double getG() {
-        return g;
-    }
+
 }
