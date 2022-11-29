f8aabacf4d56243b03f5bc940d24414c4ead5a5c
cahillBen <98129211+cahillBen@users.noreply.github.com>
2022-11-29 10:21:54 +0000

    added getters to tests for class attributes

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -8,19 +8,19 @@ public class ColourTest {
     @Test
     public void colourRedParameterTest(){
         Colour red = new Colour(1.0, 0.5, 0.0);
-        assertEquals(red.r, 255, 0.0);
+        assertEquals(red.getR(), 255, 0.0);
     }
 
     @Test
     public void colourGreenParameterTest(){
         Colour green = new Colour(0.0, 1.0, 0.0);
-        assertEquals(green.g, 255, 0.0);
+        assertEquals(green.getG(), 255, 0.0);
     }
 
     @Test
     public void colourBlueParameterTest(){
         Colour blue = new Colour(0.0, 0.0, 1.0);
-        assertEquals(blue.b, 255, 0.0);
+        assertEquals(blue.getB(), 255, 0.0);
     }
 
     @Test
@@ -42,15 +42,16 @@ public class ColourTest {
     public void colourBinaryParameter(){
         int[] array = new int[] {0b11111111, 0b11111111, 0b11111111};
         Colour white = new Colour(array);
-        assertEquals(255, white.r, 0.0);
-        assertEquals(255, white.g, 0.0);
-        assertEquals(255, white.b, 0.0);
+        assertEquals(255, white.getR(), 0.0);
+        assertEquals(255, white.getG(), 0.0);
+        assertEquals(255, white.getB(), 0.0);
     }
 
     @Test
     public void colourEquals(){
         Colour red1 = new Colour(1.0, 0.0, 0.0);
         Colour red2 = new Colour(1.0, 0.0, 0.0);
-        assertTrue(red1.equals(red2));
+        boolean ans = red1.equals(red2);
+        assertTrue(ans);
     }
 }
